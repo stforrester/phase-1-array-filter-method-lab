2b5dfffcfbec3b5a1d30364c844bb301d97e9721
Stuart Forrester <stforrester@gmail.com>
2024-11-15 03:47:47 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+
+function findMatching(array, name) {
+    return array.filter((element) => {
+        return element.toUpperCase() === name.toUpperCase();
+    });
+}
+
+function fuzzyMatch(array, letters) {
+    return array.filter((element) => {
+        return (element.slice(0, (letters.length)) === letters);
+    });
+}
+
+function matchName(array, name) {
+    return array.filter((element) => {
+        return element.name === name;
+    });
+}
+
